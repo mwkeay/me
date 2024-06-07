cdb44e446b34382e80497db0dcbe589bf8d9c872
= <mwkeay@gmail.com>
2024-06-07 18:18:53 +0100

    removed junior from title

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -29,7 +29,7 @@ const Header: FC = () => {
 
                 <div className="cv-title">
                     <h1 className={ inter.className }>Michael Keay</h1>
-                    <p className={ inter.className }>Junior Full Stack Developer</p>
+                    <p className={ inter.className }>Full Stack Developer</p>
                 </div>
 
                 <div className="cv-contact">
